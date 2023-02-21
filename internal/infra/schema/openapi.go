a03cf46d303da08e22674827c8094279191d81a6
Viacheslav Poturaev <vearutop@gmail.com>
2023-02-21 01:43:25 +0100

    Add json schema form prototype

--- internal/infra/schema/openapi.go
+++ internal/infra/schema/openapi.go
@@ -1,10 +1,17 @@
 package schema
 
 import (
+	"github.com/swaggest/jsonschema-go"
 	"github.com/swaggest/rest/openapi"
 )
 
 // SetupOpenapiCollector configures OpenAPI schema.
 func SetupOpenapiCollector(c *openapi.Collector) {
+	SetupJSONSchemaReflector(&c.Reflector().Reflector)
+
 	c.Reflector().SpecEns().Info.Title = "Photo Blog"
+}
+
+func SetupJSONSchemaReflector(r *jsonschema.Reflector) {
+	// No customizations yet.
 }
