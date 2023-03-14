fb1ce4094ef7aba7b810d5f272336c968019c7c2
Viacheslav Poturaev <vearutop@gmail.com>
2023-03-14 02:05:19 +0100

    Edit image WIP

--- internal/usecase/show_album.go
+++ internal/usecase/show_album.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"html/template"
 	"io"
+	"net/http"
 
 	"github.com/swaggest/usecase"
 	"github.com/swaggest/usecase/status"
@@ -16,6 +17,7 @@ type albumPage struct {
 	Title      string
 	Name       string
 	CoverImage string
+	NonAdmin   bool
 
 	writer io.Writer
 }
@@ -28,12 +30,19 @@ func (o *albumPage) Render(tmpl *template.Template) error {
 	return tmpl.Execute(o.writer, o)
 }
 
+type showAlbumInput struct {
+	Name    string `path:"name"`
+	hasAuth bool
+}
+
+func (i *showAlbumInput) SetRequest(r *http.Request) {
+	if r.Header.Get("Authorization") != "" {
+		i.hasAuth = true
+	}
+}
+
 // ShowAlbum creates use case interactor to show album.
 func ShowAlbum(deps getAlbumImagesDeps) usecase.Interactor {
-	type getAlbumInput struct {
-		Name string `path:"name"`
-	}
-
 	tpl, err := static.Assets.ReadFile("album.html")
 	if err != nil {
 		panic(err)
@@ -44,7 +53,7 @@ func ShowAlbum(deps getAlbumImagesDeps) usecase.Interactor {
 		panic(err)
 	}
 
-	u := usecase.NewInteractor(func(ctx context.Context, in getAlbumInput, out *albumPage) error {
+	u := usecase.NewInteractor(func(ctx context.Context, in showAlbumInput, out *albumPage) error {
 		deps.StatsTracker().Add(ctx, "show_album", 1)
 		deps.CtxdLogger().Info(ctx, "showing album", "name", in.Name)
 
@@ -66,6 +75,7 @@ func ShowAlbum(deps getAlbumImagesDeps) usecase.Interactor {
 
 		out.Title = album.Title
 		out.Name = album.Name
+		out.NonAdmin = !in.hasAuth
 		if album.CoverImage != 0 {
 			out.CoverImage = "/thumb/1200w/" + album.CoverImage.String() + ".jpg"
 		} else {
---
+++ resources/static/album.js
@@ -0,0 +1,196 @@
+function loadAlbum(albumName) {
+    "use strict";
+
+    var gpsBounds = {
+        minLat: null,
+        maxLat: null,
+        minLng: null,
+        maxLng: null
+    };
+
+
+    /**
+     * @type {Array<PhotoGps>}
+     */
+    var gpsMarkers = []
+    var client = new Backend('');
+
+    client.getAlbumImagesNameJson({
+        name: albumName
+    }, function (result) {
+        var hashByIdx = {}
+        var idxByHash = {}
+
+        for (var i = 0; i < result.images.length; i++) {
+            var img = result.images[i]
+            hashByIdx[i] = img.hash
+            idxByHash[img.hash] = i
+
+            if (typeof img.gps != 'undefined') {
+                gpsMarkers.push(img.gps)
+                if (gpsBounds.maxLat == null) {
+                    gpsBounds.maxLat = img.gps.latitude
+                    gpsBounds.minLat = img.gps.latitude
+                    gpsBounds.maxLng = img.gps.longitude
+                    gpsBounds.minLng = img.gps.longitude
+                } else {
+                    if (img.gps.longitude > gpsBounds.maxLng) {
+                        gpsBounds.maxLng = img.gps.longitude
+                    }
+                    if (img.gps.longitude < gpsBounds.minLng) {
+                        gpsBounds.minLng = img.gps.longitude
+                    }
+                    if (img.gps.latitude > gpsBounds.maxLat) {
+                        gpsBounds.maxLat = img.gps.latitude
+                    }
+                    if (img.gps.latitude < gpsBounds.minLat) {
+                        gpsBounds.minLat = img.gps.latitude
+                    }
+                }
+            }
+
+            if (typeof img.exif == "undefined" || img.exif.projection_type === "") {
+                var a = $("<a>")
+                a.attr("id", 'img' + img.hash)
+                a.attr("class", "image")
+                a.attr("href", "/image/" + img.hash + ".jpg")
+                a.attr("target", "_blank")
+
+                var srcSet = "/thumb/300w/" + img.hash + ".jpg 300w" +
+                    ", /thumb/600w/" + img.hash + ".jpg 600w" +
+                    ", /thumb/1200w/" + img.hash + ".jpg 1200w" +
+                    ", /thumb/2400w/" + img.hash + ".jpg 2400w"
+
+                var containerStyle = ""
+
+                if (img.width > 0 && img.height > 0) {
+                    srcSet += ", /image/" + img.hash + ".jpg " + img.width + "w"
+                    a.attr("data-pswp-width", img.width)
+                    a.attr("data-pswp-height", img.height)
+                    // containerStyle = "height:200px;width:"+ Math.round(img.width * 200 / img.height) + "px"
+                    containerStyle = "height:200px;width:300px"
+                    a.attr("style", containerStyle)
+                }
+
+                var img_description = '<div class="control-panel">' +
+                    '<a href="/edit/image/' + img.hash + '.html">edit image</a>' +
+                    '</div>'
+
+                if (typeof img.description !== "undefined") {
+                    img_description += "<br>" + img.description
+                }
+
+                if (typeof img.exif !== "undefined") {
+                    img_description += '<a href="#" onclick="$(this).next().toggle();return false;">toggle exif</a><div class="exif" style="display: none">';
+                    for (var k in img.exif) {
+                        if (k === "hash" || k === "exposure_time_sec" || k === "created_at") {
+                            continue;
+                        }
+
+                        var v = img.exif[k]
+
+                        if (!v) {
+                            continue;
+                        }
+
+                        img_description += "<br>" + k + ":<br> " + v;
+                    }
+                    img_description += '</div>';
+                }
+
+                a.attr("data-pswp-srcset", srcSet)
+                a.html('<div class="thumb" style="display:inline-block;background:#333;text-align:center;' + containerStyle + '">' +
+                    '<canvas id="bh-' + img.hash + '" width="32" height="32"></canvas>' +
+                    '<img alt="" src="/thumb/200h/' + img.hash + '.jpg" srcset="/thumb/400h/' + img.hash + '.jpg 2x" /></div>')
+                // a.find("img").attr("alt", img_description)
+
+
+                $(".gallery").append(a)
+                if (typeof img.blur_hash !== "undefined") {
+                    blurHash(img.blur_hash, document.getElementById('bh-' + img.hash))
+                }
+            } else {
+                var a = $("<a>")
+                a.attr("id", 'img' + img.hash)
+                a.attr("href", "/" + albumName + "/pano-" + img.hash + ".html")
+                a.html('<img alt="" src="/thumb/200h/' + img.hash + '.jpg" srcset="/thumb/400h/' + img.hash + '.jpg 2x" />')
+
+                $(".gallery-pano").show().append(a)
+            }
+        }
+
+        var lightbox = new PhotoSwipeLightbox({
+            gallery: '.gallery',
+            children: 'a.image',
+            pswpModule: PhotoSwipe
+        });
+
+        new PhotoSwipeDynamicCaption(lightbox, {
+            mobileLayoutBreakpoint: 700,
+            type: 'auto',
+            mobileCaptionOverlapRatio: 1
+        });
+
+        lightbox.init();
+
+        window.openByHash = function (hash) {
+            lightbox.loadAndOpen(idxByHash[hash], {gallery: document.querySelector('.gallery')});
+        }
+
+        if (window.location.hash) {
+            var hashname = window.location.hash.substring(1);
+
+            if (idxByHash[hashname]) {
+                window.openByHash(hashname)
+            }
+        }
+
+        lightbox.on('change', function () {
+            window.location.hash = '#' + hashByIdx[lightbox.pswp.currSlide.index]
+        })
+
+        lightbox.on('close', function () {
+            history.pushState("", document.title, window.location.pathname
+                + window.location.search);
+        })
+
+        if (gpsBounds.minLat != null) {
+            $(".gallery").append('<div id="map"></div>')
+
+            $('#map').show()
+            var map = L.map('map').fitBounds([
+                [gpsBounds.minLat, gpsBounds.minLng],
+                [gpsBounds.maxLat, gpsBounds.maxLng]
+            ]);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
+            for (var i = 0; i < gpsMarkers.length; i++) {
+                var m = gpsMarkers[i]
+                L.marker([m.latitude, m.longitude],
+                    {
+                        icon: L.icon({
+                            iconUrl: '/thumb/300w/' + m.hash + '.jpg',
+                            iconSize: [40],
+                            className: 'image-marker'
+                        })
+                    })
+                    .addTo(map)
+                    .bindPopup(
+                        L.popup()
+                            .setContent(
+                                '<p>Pos: ' + m.latitude.toFixed(6) + ',' + m.longitude.toFixed(6) + ', Alt: ' + m.altitude + 'm</p>' +
+                                '<a href="#" onclick="openByHash(\'' + m.hash + '\');return false">' +
+                                '<img style="width: 300px" src="/thumb/300w/' + m.hash + '.jpg" srcset="/thumb/600w/' + m.hash + '.jpg 2x" /></a>'
+                            )
+                    )
+            }
+        }
+    }, function (error) {
+
+    }, function (error) {
+
+    })
+}
