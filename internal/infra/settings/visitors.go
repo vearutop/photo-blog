df04c9b7064b696d552f281ca1f611935b431367
Viacheslav Poturaev <vearutop@gmail.com>
2026-02-10 00:16:41 +0100

    Extend visitors settings to reduce internal impact

--- internal/infra/settings/visitors.go
+++ internal/infra/settings/visitors.go
@@ -3,8 +3,12 @@ package settings
 import "context"
 
 type Visitors struct {
-	Tag       bool `json:"tag" inlineTitle:"Tag unique visitors with cookies." noTitle:"true"`
-	AccessLog bool `json:"access_log" inlineTitle:"Enable access log." noTitle:"true"`
+	Tag             bool     `json:"tag" inlineTitle:"Tag unique visitors with cookies." noTitle:"true"`
+	AccessLog       bool     `json:"access_log" inlineTitle:"Enable access log." noTitle:"true"`
+	IgnoreReferrers []string `json:"ignore_referrers,omitempty" title:"Ignore referrers" description:"List of referrer URL prefixes to ignore."`
+	TrustedProxies  []string `json:"trusted_proxies,omitempty" title:"Trusted proxies" description:"List of IP addresses of trusted proxies."`
+	CityDB          string   `json:"city_db" title:"City location DB" description:"Local path to DB, download and decompress from https://github.com/vearutop/ipinfo/releases/download/index/city-loc-lite.bin.zst."`
+	ASNBotDB        string   `json:"asn_bot_db" title:"ASN bot DB" description:"Local path to DB, download and decompress from https://github.com/vearutop/ipinfo/releases/download/index/asn-bot.bin.zst."`
 }
 
 func (m *Manager) SetVisitors(ctx context.Context, value Visitors) error {
