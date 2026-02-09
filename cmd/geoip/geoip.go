f2c9be15782aa693e319a22222aa19fe0d16e421
Viacheslav Poturaev <vearutop@gmail.com>
2026-02-10 00:20:38 +0100

    Generalize geoip tool

--- cmd/geoip/geoip.go
+++ cmd/geoip/geoip.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"io"
 	"log"
 	"net"
@@ -9,21 +10,28 @@ import (
 	"os"
 	"path"
 
-	"github.com/oschwald/geoip2-golang"
+	"github.com/oschwald/maxminddb-golang"
 	"github.com/swaggest/assertjson"
 )
 
 func main() {
 	// https://github.com/femueller/cloud-ip-ranges
 
-	if len(os.Args) < 2 {
+	var mmdbPath string
+	flag.StringVar(&mmdbPath, "mmdbPath", path.Join(os.TempDir()+"GeoIP2-City.mmdb"), "path to mmdb")
+
+	flag.Parse()
+
+	if flag.NArg() < 1 {
 		println("usage: " + os.Args[0] + " <IP>")
 		return
 	}
 
 	// https://github.com/P3TERX/GeoLite.mmdb
 
-	_, err := os.Stat(path.Join(os.TempDir() + "GeoIP2-City.mmdb"))
+	println(mmdbPath)
+
+	_, err := os.Stat(mmdbPath)
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
 			req, err := http.NewRequest(http.MethodGet, "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb", nil)
@@ -38,7 +46,7 @@ func main() {
 
 			defer resp.Body.Close()
 
-			f, err := os.Create(path.Join(os.TempDir() + "GeoIP2-City.mmdb"))
+			f, err := os.Create(mmdbPath)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -49,17 +57,22 @@ func main() {
 		}
 	}
 
-	db, err := geoip2.Open(path.Join(os.TempDir() + "GeoIP2-City.mmdb"))
+	db, err := maxminddb.Open(mmdbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer db.Close()
 	// If you are using strings that may be invalid, check that ip is not nil
-	ip := net.ParseIP(os.Args[1])
-	record, err := db.City(ip)
+	ip := net.ParseIP(flag.Arg(0))
+
+	var res any
+
+	err = db.Lookup(ip, &res)
 	if err != nil {
 		log.Fatal(err)
 	}
-	j, _ := assertjson.MarshalIndentCompact(record, "", " ", 120)
+
+	j, _ := assertjson.MarshalIndentCompact(res, "", " ", 120)
 	println(string(j))
 }
