e692b94deb81f16de4822b128677230b63653062
Viacheslav Poturaev <vearutop@gmail.com>
2026-02-10 00:13:51 +0100

    Tweak user agent bot detection

--- pkg/webstats/user_agent.go
+++ pkg/webstats/user_agent.go
@@ -19,21 +19,16 @@ func IsBot(ua string) bool {
 		return true
 	}
 
+	if strings.Contains(ua, "Bytespider") {
+		return true
+	}
+
 	// Mozilla/5.0 (compatible; CensysInspect/1.1; +https://about.censys.io/)
 	if strings.Contains(ua, "+http") {
 		return true
 	}
 
 	if strings.Contains(ua, "Headless") {
-		return true
-	}
-
-	// Mozilla/5.0 (Linux; Android 5.0) AppleWebKit/537.36 (KHTML, like Gecko) Mobile Safari/537.36 (compatible; Bytespider; spider-feedback@bytedance.com)
-	if strings.Contains(ua, "Bytespider") {
-		return true
-	}
-
-	if ua == "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/95.0" {
 		return true
 	}
 
