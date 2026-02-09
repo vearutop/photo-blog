5cd43bc2a9357036e61a598f04f57a8464a91e53
Viacheslav Poturaev <vearutop@gmail.com>
2026-02-10 00:23:10 +0100

    Skip stats for admin and bots

--- internal/usecase/add_stats.go
+++ internal/usecase/add_stats.go
@@ -17,7 +17,12 @@ type collectStatsDeps interface {
 
 func CollectStats(deps collectStatsDeps) usecase.Interactor {
 	u := usecase.NewInteractor(func(ctx context.Context, input visitor.CollectStats, output *struct{}) error {
-		deps.CtxdLogger().Info(ctx, "stats", "input", input, "admin", auth.IsAdmin(ctx))
+		deps.CtxdLogger().Info(ctx, "stats", "input", input,
+			"admin", auth.IsAdmin(ctx), "bot", auth.IsBot(ctx))
+
+		if auth.IsBot(ctx) || auth.IsAdmin(ctx) {
+			return nil
+		}
 
 		deps.VisitorStats().CollectRequest(ctx, input, time.Now())
 
