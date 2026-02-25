24c358f4b49d34348780da20c4d43cc94cfbeb0b
Viacheslav Poturaev <vearutop@gmail.com>
2026-02-25 22:16:15 +0100

    Add canonical base URL setting

--- internal/infra/settings/appearance.go
+++ internal/infra/settings/appearance.go
@@ -23,9 +23,10 @@ type Appearance struct {
 
 	FeaturedAlbumName string `split_words:"true" default:"featured" json:"featured_album_name" title:"Featured album name" description:"The name of an album to show on the main page."`
 
-	Languages    []string `json:"languages" title:"Languages" description:"Supported content languages." items.title:"Language code"`
-	ThumbBaseURL string   `json:"thumb_base_url" title:"Thumbnails Base URL" description:"Optional custom URL for thumbnails. Example: https://example.org/thumb"`
-	ImageBaseURL string   `json:"image_base_url" title:"Images Base URL" description:"Optional custom URL for images. Example: https://example.org/image"`
+	Languages        []string `json:"languages" title:"Languages" description:"Supported content languages." items.title:"Language code"`
+	ThumbBaseURL     string   `json:"thumb_base_url" title:"Thumbnails Base URL" description:"Optional custom URL for thumbnails. Example: https://example.org/thumb"`
+	ImageBaseURL     string   `json:"image_base_url" title:"Images Base URL" description:"Optional custom URL for images. Example: https://example.org/image"`
+	CanonicalBaseURL string   `json:"canonical_base_url" title:"Canonical Base URL" description:"Optional canonical base URL for site. Example: https://example.org/"`
 
 	MainMenu     []MenuItem   `json:"main_menu,omitempty" title:"Main Menu"`
 	TextReplaces txt.Replaces `json:"text_replaces,omitempty" title:"Text Replaces"`
